4639d3aaf6ae82d342e7a8a890fa5133285aa162
zhixian <zhixian@hotmail.com>
2020-07-09 08:27:10 +0800

    Define ScanDataRecord

---
+++ android/src/main/java/com/emptool/tea/ScanDataRecord.java
@@ -0,0 +1,8 @@
+package com.emptool.tea;
+
+public class ScanDataRecord {
+    public int Id;
+    public String Tag;
+    public String Data;
+    public String CreDt;
+}
--- android/src/main/java/com/emptool/tea/TeaDbHelper.java
+++ android/src/main/java/com/emptool/tea/TeaDbHelper.java
@@ -1,10 +1,15 @@
 package com.emptool.tea;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import android.util.Log;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase;
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
 
 
 public class TeaDbHelper extends SQLiteOpenHelper {
@@ -67,4 +72,106 @@ public class TeaDbHelper extends SQLiteOpenHelper {
 
         Log.v(LOG_TAG, "scan_data newRowId is " + Long.toString(newRowId));
     }
+
+    public long GetCount(String status)
+    {
+        String selection = "status = ?";
+        String[] selectionArgs = { status };
+
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        long count = DatabaseUtils.queryNumEntries(
+            db, "scan_data", selection, selectionArgs);
+        db.close();
+        return count;
+    }
+
+    public ArrayList GetLoanRecord(String status)
+    {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        // Define a projection of columns from the database, that you use from query.
+        String[] projection = {
+            "id",
+            "tag",
+            "data",
+            "cre_dt"
+        };
+
+        // Filter results WHERE "title" = 'My Title'
+        String selection = "status = ?";
+        String[] selectionArgs = { status };
+
+        // How you want the results sorted in the resulting Cursor
+        String sortOrder = "id ASC";
+
+        Cursor cursor = db.query(
+            "scan_data",                     // The table to query
+            projection,                               // The columns to return
+            selection,                                // The columns for the WHERE clause
+            selectionArgs,                            // The values for the WHERE clause
+            null,                                     // don't group the rows
+            null,                                     // don't filter by row groups
+            sortOrder                                 // The sort order
+            );
+
+        List items = new ArrayList<>();
+        while (cursor.moveToNext()) {
+            // public class ScanDataRecord {
+            // public int Id;
+            // public String Tag;
+            // public String Data;
+            // public String CreDt;
+
+            ScanDataRecord newRecord = new ScanDataRecord();
+            newRecord.Id = cursor.getInt(cursor.getColumnIndexOrThrow("id"));
+            newRecord.Tag = cursor.getString(cursor.getColumnIndexOrThrow("tag"));
+            newRecord.Data = cursor.getString(cursor.getColumnIndexOrThrow("data"));
+            newRecord.CreDt = cursor.getString(cursor.getColumnIndexOrThrow("cre_dt"));
+
+
+            //String itemId = cursor.getString(cursor.getColumnIndexOrThrow(0));
+            //itemIds.add(itemId);
+            items.add(newRecord);
+        }
+
+        cursor.close();
+
+        return (ArrayList)items;
+    }
+
+
+    public void Update(int id)
+    {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // New value for one column
+        String title = "MyNewTitle";
+        ContentValues values = new ContentValues();
+        values.put("status", 1);
+
+        // Which row to update, based on the title
+        String selection = "id = ?";
+        String[] selectionArgs = { id };
+
+        int count = db.update(
+            "scan_data",
+            values,
+            selection,
+            selectionArgs);
+    }
+
+    public void ClearRecords()
+    {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // Define 'where' part of query.
+        String selection = "status = ?";
+        // Specify arguments in placeholder order.
+        String[] selectionArgs = { "1" };
+        
+        // Issue SQL statement.
+        int deletedRows = db.delete("scan_data", selection, selectionArgs);
+    }
+
 }
